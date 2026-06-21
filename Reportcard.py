a66f4963d857447043f927b97b08764fb2c717d3
BasicCoder89 <112031667+BasicCoder89@users.noreply.github.com>
2026-06-21 15:10:07 -0400

    Add initial Reportcard.py with student details

---
+++ Reportcard.py
@@ -0,0 +1,12 @@
+name = 'Michael'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 19
+print(age, type(age))
+
+score = 89.5
+print(isinstance(score, float))
+print(score, type(score))
